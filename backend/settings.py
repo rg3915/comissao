1045d3384f130ab75fa0ea181b03ed47da0d41dc
Regis Santos <regis42santos@gmail.com>
2022-06-07 03:19:00 -0300

    config CORS headers

--- backend/settings.py
+++ backend/settings.py
@@ -29,6 +29,9 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:8080',
+]
 
 # Application definition
 
@@ -45,6 +48,7 @@ INSTALLED_APPS = [
     'django_extensions',
     'widget_tweaks',
     'drf_yasg',
+    'corsheaders',
     # minhas apps
     'backend.crm',
     'backend.financial',
@@ -54,6 +58,8 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    # corsheaders
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
